1895c9f6b8827d3653635abe381e8388c892e523
snarek <snarek@yandex.ru>
2016-02-19 17:34:55 +0400

    1st

--- index.php
+++ index.php
@@ -3,11 +3,22 @@
   <head>
     <meta charset="utf-8">
     <title>Կայք</title>
-    <link href="/css/style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
   <body>
       <header>
-          
+          <div id="logo">
+            <a href="" title="Մտնել գլխավոր էջ"><span>Գ</span>լխավոր</a>
+          </div>
+          <div id="menuHead">
+            <a href="/about.php">
+                <div style="margin-right: 5%;">Մեր մասին</div>
+            </a>
+            <a href="/feedback.php">
+                <div>Հետադարձ կապ</div>
+            </a>
+          </div>
+          <div id="regAuth"><a href="/auth.php">Մուտք</a> | <a href="/reg.php">Գրանցվել</a></div>
       </header>
   </body>
 </html>
