8fc730a428fc7bc924b8592f2b26922759109c09
snarek <snarek@yandex.ru>
2016-02-22 14:13:42 +0400

    Summary

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Կայք</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
   </head>
   <body>
       <header>
@@ -18,7 +18,27 @@
                 <div>Հետադարձ կապ</div>
             </a>
           </div>
-          <div id="regAuth"><a href="/auth.php">Մուտք</a> | <a href="/reg.php">Գրանցվել</a></div>
+          <div id="regAuth">
+            <a href="/auth.php">Մուտք</a> |
+            <a href="/reg.php">Գրանցվել</a>
+          </div>
       </header>
+
+      <footer>
+        <div id="social">
+          <a href="http://www.vk.com">
+            <img src="img/vk.png" title="VK" alt="Vkontakte"/>
+          </a>
+          <a href="http://www.fb.com">
+            <img src="img/fb.png" title="FB" alt="Facebook"/>
+          </a>
+          <a href="http://www.twitter.com">
+            <img src="img/twitter.png" title="Twitter" alt="Twitter"/>
+          </a>
+        </div>
+        <div id="rights">
+          Բոլոր իրավունքները պաշտպանված են  &copy; <?= date('Y') ?>
+        </div>
+      </footer>
   </body>
 </html>
