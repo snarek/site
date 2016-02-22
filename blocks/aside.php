7e91de87331e3b0cfb854d6ffbd994436f3aba44
snarek <snarek@yandex.ru>
2016-02-22 17:03:13 +0400

    Summary

---
+++ blocks/aside.php
@@ -0,0 +1,8 @@
+<aside>
+  <div class="banner">
+    <img src="img/banner_1.png" alt="Banner 1" title="Banner 1" />
+  </div>
+  <div class="banner">
+    <img src="img/banner_2.png" alt="Banner 2" title="Banner 2" />
+  </div>
+</aside>
--- index.php
+++ index.php
@@ -1,44 +1,39 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <title>Կայք</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <?php
+      $title="Կայք";
+      require_once "/blocks/head.php";
+    ?>
   </head>
   <body>
-      <header>
-          <div id="logo">
-            <a href="" title="Մտնել գլխավոր էջ"><span>Գ</span>լխավոր</a>
-          </div>
-          <div id="menuHead">
-            <a href="/about.php">
-                <div style="margin-right: 5%;">Մեր մասին</div>
-            </a>
-            <a href="/feedback.php">
-                <div>Հետադարձ կապ</div>
-            </a>
-          </div>
-          <div id="regAuth">
-            <a href="/auth.php">Մուտք</a> |
-            <a href="/reg.php">Գրանցվել</a>
-          </div>
-      </header>
-
-      <footer>
-        <div id="social">
-          <a href="http://www.vk.com">
-            <img src="img/vk.png" title="VK" alt="Vkontakte"/>
-          </a>
-          <a href="http://www.fb.com">
-            <img src="img/fb.png" title="FB" alt="Facebook"/>
-          </a>
-          <a href="http://www.twitter.com">
-            <img src="img/twitter.png" title="Twitter" alt="Twitter"/>
-          </a>
-        </div>
-        <div id="rights">
-          Բոլոր իրավունքները պաշտպանված են  &copy; <?= date('Y') ?>
-        </div>
-      </footer>
+      <?php require_once "/blocks/header.php" ?>
+      <div id="wrapper">
+        <section>
+          <article class="bigArticle">
+            <img src="img/articles/1.jpg" alt="Article 1" title="Article 1" />
+            <h2>Վերնագիր</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a interdum risus. In mauris sem, finibus id eleifend eget, accumsan non dui. Suspendisse sodales purus et lacus rhoncus lacinia. Morbi volutpat mauris non lorem porttitor vulputate. Aliquam in nulla sem. Integer eu efficitur dui, eget bibendum sapien. Nulla vitae venenatis ligula, a mollis quam. Donec tempor lorem sit amet sollicitudin sodales. Etiam viverra magna nec odio gravida, id commodo lacus tempor. Nam suscipit dolor ut enim eleifend, convallis consequat nisl pulvinar. Nunc nibh nisi, efficitur id lorem sit amet, hendrerit viverra sem.
+            </p>
+            <a href="article.php"><div>Շարունակել</div></a>
+          </article>
+          <article class="smallArticle">
+            <img src="img/articles/2.png" alt="Article 2" title="Article 2" />
+            <h2>Վերնագիր</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a interdum risus. In mauris sem, finibus id eleifend eget, accumsan non dui. Suspendisse sodales purus et lacus rhoncus lacinia. Morbi volutpat mauris non lorem porttitor vulputate. Aliquam in nulla sem.
+            </p>
+            <a href="article.php"><div>Շարունակել</div></a>
+          </article>
+          <article class="smallArticle">
+            <img src="img/articles/3.jpg" alt="Article 3" title="Article 3" />
+            <h2>Վերնագիր</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a interdum risus. In mauris sem, finibus id eleifend eget, accumsan non dui. Suspendisse sodales purus et lacus rhoncus lacinia. Morbi volutpat mauris non lorem porttitor vulputate. Aliquam in nulla sem.
+            </p>
+            <a href="article.php"><div>Շարունակել</div></a>
+          </article>
+        </section>
+        <?php require_once "/blocks/aside.php" ?>
+      </div>
+      <?php require_once "/blocks/footer.php" ?>
   </body>
 </html>
